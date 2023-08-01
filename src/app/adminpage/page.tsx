9b2ebeb4de1e2c07d71543baaad5b86518ff5d21
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-08-01 22:57:00 +0700

    sliced admin page

--- src/app/adminpage/page.tsx
+++ src/app/adminpage/page.tsx
@@ -10,7 +10,7 @@ export default function AdnminPage() {
   const [index, setIndex] = useState(0);
   return (
     <>
-      <div className="w-full h-screen bg-[#F3EEE7] flex">
+      <div className="w-auto h-screen bg-[#F3EEE7] flex">
         <div className="w-1/6 flex flex-col items-center bg-[#379392] py-8 px-4">
           <Image
             src="/images/adminpage/bist.svg"
@@ -44,7 +44,7 @@ export default function AdnminPage() {
           </div>
         </div>
         <div className="w-5/6 px-14 py-9 flex flex-col">
-          <div className="flex justify-end text-[#379392] items-center gap-1 mb-10">
+          <div className="flex justify-end text-[#379392] items-center gap-1 mb-1">
             <p className="text-[#379392] text-[24px]">
               Hi, <span className="font-bold">Admin!</span>
             </p>
