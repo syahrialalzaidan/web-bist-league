3f74941eff6fe88f7a9024f575650933e97c672f
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-08-07 11:58:13 +0700

    rapihin submission page

--- src/app/submission/page.tsx
+++ src/app/submission/page.tsx
@@ -3,11 +3,9 @@ import Submission from "./components/Submission";
 export default function SubmissionPage() {
   return (
     <div className="bg-[url('/images/compregister/bg-comp-mobile.svg')] lg:bg-[url('/images/compregister/bg-comp-web.svg')] bg-cover min-h-screen flex flex-col">
-      <div className="bg-[url('/images/compregister/left.svg')] lg:bg-[url('/images/compregister/left-web.svg')] bg-no-repeat lg:bg-left-bottom bg-fixed flex-1 flex flex-col">
         <div className="bg-[url('/images/compregister/right.svg')] lg:bg-[url('/images/compregister/right-web.svg')] bg-no-repeat bg-right-bottom lg:bg-right-top flex-1">
           <Submission />
         </div>
-      </div>
     </div>
   );
 }
