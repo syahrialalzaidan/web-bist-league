cd8c731dc581e274f7c7c852c15d7cdcd568a293
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-08-07 14:51:15 +0700

    dashboard user -competition

---
+++ src/app/dashboarduser/page/profileuser.tsx
@@ -0,0 +1,154 @@
+import NavUser from "../component/nav";
+
+export default function ProfilUser() {
+  return (
+    <>
+      <div className="w-full flex flex-col lg:flex-row bg-[#F3EEE7]">
+        <NavUser active={1} />
+        <div className="w-full px-6 py-6 lg:px-16 lg:py-10 flex flex-col items-center lg:items-start gap-4">
+          <p className="text-[24px] lg:text-[32px] font-extrabold">Personal Information</p>
+          <div className="w-full bg-white flex flex-col items-center rounded-lg gap-8 px-5 lg:px-9 py-4 lg:py-8 mb-8">
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="fullname" className="font-extrabold">
+                  Fullname
+                </label>
+                <input
+                  type="text"
+                  id="fullname"
+                  placeholder="Please enter your fullname"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="username" className="font-extrabold">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="Please enter your username"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="email" className="font-extrabold">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="Please enter your email"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="age" className="font-extrabold">
+                  Age
+                </label>
+                <input
+                  type="text"
+                  id="age"
+                  placeholder="Please enter your age"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="address" className="font-extrabold">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  placeholder="Please enter your address"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="phone number" className="font-extrabold">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  id="phone number"
+                  placeholder="Please enter your phone number"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="institution" className="font-extrabold">
+                  Institution
+                </label>
+                <input
+                  type="text"
+                  id="institution"
+                  placeholder="Please enter your institution"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="linkedin" className="font-extrabold">
+                  LinkedIn
+                </label>
+                <input
+                  type="text"
+                  id="linkedin"
+                  placeholder="Please enter your linkedin"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="major" className="font-extrabold">
+                  Major
+                </label>
+                <input
+                  type="text"
+                  id="major"
+                  placeholder="Please enter your major"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="idline" className="font-extrabold">
+                  Id Line
+                </label>
+                <input
+                  type="text"
+                  id="idline"
+                  placeholder="Please enter your idline"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row lg:justify-center gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="entry year" className="font-extrabold">
+                  Entry Year
+                </label>
+                <input
+                  type="text"
+                  id="entry year"
+                  placeholder="Please enter your entry year"
+                  className="w-full lg:w-72 xl:w-96 border-2 border-[#BDBDBD] rounded-sm px-2 lg:px-5 py-3"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex justify-center">
+            <button type = "submit" className="flex justify-center items-center text-white bg-[#F8A22D] rounded-lg px-10 py-3">
+                Save Personal Information
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import LoginPage from './login/page'
 import PaymentVerification from './adminpage/component/payment'
 import UserVerification from './adminpage/component/user'
 import AdnminPage from './adminpage/page'
+import NavUser from './dashboarduser/component/nav'
+import ProfilUser from './dashboarduser/page/profileuser'
+import OverviewUser from './dashboarduser/page/overview'
 
 export default function Home() {
   return (
-    <AdnminPage/>
+    <ProfilUser/>
   )
 }
