baa0f70aec268c13c99d2fbd5aad956d1d47d09f
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-08-11 12:06:46 +0700

    feat: Add cookies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Manrope } from "next/font/google";
-import { LoginContextProvider } from "./context/Logincontext";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -21,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={manrope.className}>
-        <LoginContextProvider>{children}</LoginContextProvider>
+        {children}
       </body>
     </html>
   );
