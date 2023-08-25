711c477e7e5ee73a86338e04b429c0c103ecc521
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-08-25 22:17:24 +0700

    feat: Add api submission, tinggal get submission masih error

--- src/app/submission/components/Submission.tsx
+++ src/app/submission/components/Submission.tsx
@@ -1,19 +1,104 @@
 "use client"
 import { useState } from "react";
 import { AiOutlineArrowLeft, AiFillCheckCircle } from "react-icons/ai";
+import axios from "axios";
 
 import Image from "next/image";
 
 import BulatKiri from "@images/submission/left.svg";
 
-export default function Submission() {
-    const [selectedFile, setSelectedFile] = useState("No file selected");
-    const [submittedFile, setSubmittedFile] = useState("Empty");
+interface TeamMember {
+    user_id: string;
+    username: string;
+    fullname: string;
+    is_leader: boolean;
+}
 
-    const fileChangedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+interface TeamData {
+    team_id: string;
+    team_name: string;
+    team_redeem_code: string;
+    is_active: boolean;
+    payment_proof: string;
+    payment_proof_url: string;
+    payment_status: string;
+    payment_status_code: number;
+    student_card: string;
+    student_card_url: string;
+    student_card_status: string;
+    student_card_status_code: number;
+    self_portrait: string;
+    self_portrait_url: string;
+    self_portrait_status: string;
+    self_portrait_status_code: number;
+    twibbon: string;
+    twibbon_url: string;
+    twibbon_status: string;
+    twibbon_status_code: number;
+    enrollment: string;
+    enrollment_url: string;
+    enrollment_status: string;
+    enrollment_status_code: number;
+    members: TeamMember[];
+}
+
+interface SubmittedData {
+    doc_type: string
+    document: string
+    document_name: string
+}
+
+interface SubmissionData {
+    team_id: string
+    doc_type: string
+    submission_filename: string
+    submission_url: string
+    submission_lastupdate: string
+}
+
+interface SubmissionProps {
+    teamData: TeamData;
+    submissionData: SubmissionData;
+    jwt_token: string;
+}
+
+export default function Submission({ teamData,
+    submissionData,
+    jwt_token,
+}: SubmissionProps) {
+    const [selectedFile, setSelectedFile] = useState("No file selected");
+    const [base64, setBase64] = useState("");
+
+    const fileChangedHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
         if (!e.target.files) return;
+        const reader = new FileReader();
         setSelectedFile(e.target.files[0].name)
-        setSubmittedFile(e.target.files[0].name)
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = () => {
+            if (typeof reader.result === "string") {
+                const base64Content = reader.result.split(",")[1]
+                setBase64(base64Content)
+            }
+        }
+    }
+
+    const submitFile = async () => {
+        const submittedData: SubmittedData = {
+            doc_type: "submission_1",
+            document: base64,
+            document_name: selectedFile
+        }
+
+        try {
+            const submit = await axios.post("https://be-staging-b6utdt2kwa-et.a.run.app/team/document", submittedData, {
+                headers: {
+                    Authorization: `Bearer ${jwt_token}`,
+                }
+            })
+            setSelectedFile("No file selected")
+        } catch (err) {
+            console.log(err)
+        }
     }
     return (
         <div className="px-5 py-7">
@@ -27,7 +112,7 @@ export default function Submission() {
 
             <div className="flex items-center justify-center my-8 ">
                 <div className="bg-white py-6 px-5 rounded-lg max-w-[540px] z-10">
-                    {submittedFile === "Empty" ? (
+                    {!submissionData ? (
                         <h1 className="text-center text-sm lg:text-xl py-16 px-3 lg:px-10 lg:py-0 lg:pt-16 lg:pb-28 ">
                             Preliminary submission must be submitted in PDF format with a maximum size of 10MB.
                         </h1>
@@ -42,19 +127,19 @@ export default function Submission() {
                             <h1 className="text-center text-sm lg:text-xl">
                                 Last submission:
                             </h1>
-                            <h1 className="text-center text-sm font-bold lg:text-xl mb-6">October 1st, 2023, 23:59</h1>
+                            <h1 className="text-center text-sm font-bold lg:text-xl mb-6">{submissionData.submission_lastupdate}</h1>
                         </div>
                     )}
 
-                    <form>
+                    <form onSubmit={submitFile}>
                         <div className="border border-[#BDBDBD] mb-4 lg:mx-16 p-3.5 rounded text-sm font-medium flex items-center justify-between">
-                            <span className={submittedFile === "Empty" ? "text-[#BDBDBD]" : "text-[#413687]"}>
-                                {submittedFile}
+                            <span className={!submissionData ? "text-[#BDBDBD]" : "text-[#413687]"}>
+                                {submissionData ? submissionData.submission_filename : "Empty"}
                             </span>
-                            <AiFillCheckCircle className={submittedFile === "Empty" ? "hidden" : "inline-block ml-2 text-[#27ae60] w-6 h-6"} />
+                            <AiFillCheckCircle className={!submissionData ? "hidden" : "inline-block ml-2 text-[#27ae60] w-6 h-6"} />
                         </div>
                         <div className="flex flex-row items-center border lg:mx-16 mb-6 border-[#BDBDBD] stroke-1 rounded justify-between pr-1 pl-2.5 py-1.5">
-                            <label className="text-sm text-[#4F4F4F] font-medium">No file selected</label>
+                            <label className="text-sm text-[#4F4F4F] font-medium">{selectedFile}</label>
                             <input
                                 type="file"
                                 id="custom-input"
@@ -65,7 +150,7 @@ export default function Submission() {
                                 htmlFor="custom-input"
                                 className="block text-white ml-4 py-2.5 px-5 rounded-md border-0 text-sm font-semibold bg-[#413687] cursor-pointer"
                             >
-                                {submittedFile === "Empty" ? "Upload" : "Reupload"}
+                                {!submissionData ? "Upload" : "Reupload"}
                             </label>
                         </div>
                         <div className="mb-4 lg:mx-16 rounded text-sm font-bold flex items-center">
